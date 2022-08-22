288ae37b7a8980b8c80ff2ac8b6eed71e93112d9
David Wengier <david.wengier@microsoft.com>
2022-08-22 13:50:58 +1000

    Initial commit

---
+++ Helpers.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+
+namespace EnCTestApp;
+
+public static class Helpers
+{
+    public static void PrintMembersWithParameters()
+    {
+        int row = 2;
+        int col = 50;
+        var t = typeof(Program);
+        foreach (var m in t.GetMethods(System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.DeclaredOnly))
+        {
+            Console.SetCursorPosition(col, ++row);
+            Console.Write($"Method '{m.Name}'");
+            Console.SetCursorPosition(col, ++row);
+            Console.Write($"  Return Type: '{m.ReturnType.Name}'");
+
+            Console.SetCursorPosition(col, ++row);
+            Console.Write($"  Parameters: {m.GetParameters().Length}");
+
+            foreach (var p in m.GetParameters())
+            {
+                Console.SetCursorPosition(col, ++row);
+                Console.Write($"   - {p.Name}: {p.ParameterType.Name}");
+            }
+        }
+    }
+
+    public static string GetMethodName()
+    {
+        return new StackTrace().GetFrame(1)?.GetMethod()?.Name ?? "(null)";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+using static EnCTestApp.Helpers;
+
+namespace EnCTestApp;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.Clear();
+            Console.SetCursorPosition(0, 0);
+            Console.WriteLine(DateTime.Now.ToString());
+            Console.WriteLine();
+
+            M();
+
+            PrintMembersWithParameters();
+
+            Thread.Sleep(500);
+        }
+    }
+
+    public static void M()
+    {
+        Console.WriteLine(M2().ToString());
+
+        //string L(int x, long y)
+        //{
+        //    return $"""
+        //        x is:
+        //            - {x.GetType().Name}
+        //            - equal to '{x}'
+
+        //        """ +
+        //             $"""
+        //        y is
+        //            - {y.GetType().Name}
+        //            - equal to '{y}'
+
+        //        """ +
+        //             $"""
+        //        this method is
+        //            - named '{GetMethodName()}'
+        //        """;
+        //}
+    }
+
+    private static string M2()
+    {
+        var sb = new StringBuilder();
+        sb.Append($"""
+            this method is
+                - named '{GetMethodName()}'
+            """);
+        return sb.ToString();
+    }
+}
